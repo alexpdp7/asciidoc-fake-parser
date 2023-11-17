f040d299eb748cd50cff01e5cd6bc3ee5dff4237
alex <alex@pdp7.net>
2023-11-17 13:31:42 +0100

    bogus: make things simpler

--- afp/utils/grep.py
+++ afp/utils/grep.py
@@ -14,19 +14,13 @@ def main():
 
     patterns = [re.compile(p) for p in args.pattern]
 
-    unparsed = []
-
     for file in args.file:
         try:
             parsed = afp.asciidoc_fake_parse(file, silence_asciidoctor=True)
         except:
-            unparsed.append(file)
+            print("Error parsing:", str(file), file=sys.stderr, flush=True)
             continue
         for elem in parsed:
-            matched = []
             for pattern in patterns:
                 for match in pattern.findall(elem["text"]):
                     print("\t".join(map(str, [elem["path"], str(file), elem["start"], match, repr(elem["text"])])))
-
-    if unparsed:
-        print("Error parsing:", ", ".join(list(map(str, unparsed))), file=sys.stderr, flush=True)
